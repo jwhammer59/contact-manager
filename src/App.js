015ed5b8d011b3c8e5224a3d38fc8bdc3a0e3df1
Jeff <jeff@jhammermedia.com>
2018-08-11 09:20:33 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Contacts from './components/Contacts';
+import Header from './components/Header';
+
+import { Provider } from './context';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <Contacts />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
